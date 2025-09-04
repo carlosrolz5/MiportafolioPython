a84d8c72c45cd71ee646624b137f577f2db3b099
carlosrolz5 <carlosrolz2005@gmail.com>
2025-09-03 19:16:46 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,26 +1,24 @@
+#titulo
 print("*" * 40)
 print("BIENVENIDO A LA CALCULADORA DE COMISIONES")
 print("*" * 40)
 
-while True:
-    try:
-        venta = float(input("Ingree el monto de la venta: "))
-        p_comision = float(input("Ingrese el porcentaje de la comisión: "))
-        comision = (venta * p_comision) / 100
-        total = comision + venta
-        if(venta < 0  or p_comision < 0):
-            print("No pueden ingresarse valores o menores a cero")
-            continue
+#ingresar datos de la venta y el porcentaje de la comision
+venta = float(input("Ingrese el monto de la venta: "))
+p_comision = float(input("Ingrese el porcentaje de la comisión: "))
+#calculos
+comision = (venta * p_comision) / 100
+total = comision + venta
 
-    except ValueError:
-        print("No ingresaste un valor flotante")
-        continue
 
-    print("*" * 40)
-    print("Resultados: ")
-    print("*" * 40)
-    print(f"Monto: ${venta:.2f}")
-    print(f"Porcentaje de comision: {p_comision:.2f}%")
-    print(f"Comision: ${comision:.2f}")
-    print(f"Total: ${total:.2f}")
-    print("*" * 40)
+
+
+#Mostrar resultados rondeados a dos decimales
+print("*" * 40)
+print("Resultados: ")
+print("*" * 40)
+print(f"Monto: ${venta:.2f}")
+print(f"Porcentaje de comision: {p_comision:.2f}%")
+print(f"Comision: ${comision:.2f}")
+print(f"Total: ${total:.2f}")
+print("*" * 40)
