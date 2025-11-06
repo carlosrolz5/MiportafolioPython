7ae5ff9cc41ad6a09db42de76911e7deaa499e15
carlosrolz5 <carlosrolz2005@gmail.com>
2025-11-05 21:26:26 -0600

    Update and rename proyectosemana1.py to analizadortexto.py

--- analizadortexto.py
+++ analizadortexto.py
@@ -1,30 +1,33 @@
-
+#ingresar nombre y apellido
 nombre = input("Ingresa tu nombre: ")
 apellido = input("Ingresa tu apellido: ")
-
+#registrar nombre completo
 nom_completo = nombre + " " + apellido
-print("Tu nombre es: " + nom_completo)
+#imprimir nombre completo
+print("Tu nombre es: " + nom_completo)}
+#imprimir iniciales
 print("Tus iniciales son: " + nombre[0] + ". " + apellido[0])
+#version divertida (nombre invertido y apellido invertido)
 print("Una version divertida de tu nombre es: " + nombre[::-1] + " " + apellido[::-1])
+"inicializar variable de nombre en mayuscula
 nom_mayuscula = ""
+#recorrer nombre completo y agregar cada letra en mayuscula a la variable nom_mayuscula
 for letra in nom_completo:
     nom_mayuscula += letra.upper()
+#imprimir nombre en mayuscula
 print("Nombre en mayusculas :", nom_mayuscula)
+#variable con todas las letras del abecedario
 letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+#contador que cuenta la cantidad de letras del nombre completo
 cant_letras = 0
+#se recorre cada letra del nombre, se convierte a mayuscula para compararla con la variable de letras y se incrementa el contador
 for nom in nom_completo:
     for letra in nom:
         if letra.upper() in letras:
             cant_letras += 1
-print("Cantidad de letras en tu nombre: ", cant_letras)
-""""
-def contar_letras(nombre, cant_letras):
-    for letra in nombre:
-        if (letra.upper() in letras):
-            cant_letras += 1
-    return cant_letras
-"""
-#print(f"Cantidad de letras de tu nombre : {contar_letras(nombre, cant_letras) + contar_letras(apellido, cant_letras)}")
 
-print("Apellido y nombre: " + apellido + ", " + nombre)
+print("Cantidad de letras en tu nombre: ", cant_letras)
+
+
 
